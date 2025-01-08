54fcc4625f579c154361af71e1fd13d4a259247b
Satish Ghode <satishghode9075@gmail.com>
2025-01-08 23:12:31 +0530

    stater_code

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Home from './page/Home/Home.jsx'
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import Login from './page/Login/Login.jsx';
+import Register from './page/Register/Register.jsx';
+import NotFound from './page/NotFound/NotFound.jsx';
+import About from './page/About/About.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path='/register' element={<Register/>} />
+      <Route path='/about' element={<About />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>,
+)
